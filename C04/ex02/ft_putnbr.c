6e9921518199c24edc30d9b472ab7a73833a5dd6
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2022-10-18 13:02:21 +0200

    Update ft_putnbr.c

--- C04/ex02/ft_putnbr.c
+++ C04/ex02/ft_putnbr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luclopez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: luclopez <luclopez@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 09:35:00 by luclopez          #+#    #+#             */
 /*   Updated: 2022/08/30 20:12:12 by luclopez         ###   ########.fr       */
