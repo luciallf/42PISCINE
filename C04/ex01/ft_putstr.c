dfbc494d42e3791bd246f45a067f4aca122bc0be
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2022-10-18 13:01:53 +0200

    Update ft_putstr.c

--- C04/ex01/ft_putstr.c
+++ C04/ex01/ft_putstr.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luclopez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: luclopez <luclopez@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 09:29:13 by luclopez          #+#    #+#             */
 /*   Updated: 2022/08/29 18:18:12 by luclopez         ###   ########.fr       */
