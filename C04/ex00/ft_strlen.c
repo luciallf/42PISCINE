3c87880eb481887903e15faa04684ab1c72b652e
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2022-10-18 13:01:07 +0200

    Update ft_strlen.c

--- C04/ex00/ft_strlen.c
+++ C04/ex00/ft_strlen.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luclopez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: luclopez <luclopez@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 09:08:25 by luclopez          #+#    #+#             */
 /*   Updated: 2022/08/29 09:46:00 by luclopez         ###   ########.fr       */
