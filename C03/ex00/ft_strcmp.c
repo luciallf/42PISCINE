2b8c518e3f8d3a3555c1908f7c04eead6c857c25
Lucía López Fuentes <108120620+luciallf@users.noreply.github.com>
2022-10-18 12:58:33 +0200

    Update ft_strcmp.c

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: luclopez <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: luclopez <luclopez@student.42malaga.c      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 08:26:38 by luclopez          #+#    #+#             */
 /*   Updated: 2022/08/29 10:04:25 by luclopez         ###   ########.fr       */
